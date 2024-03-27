93cb0d683bfec1c975dbe0bcbec2655594e635ad
arneyva <princethereal1416@gmail.com>
2024-03-27 09:37:05 +0700

    first commit

---
+++ src/pages/auth/login.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { Button, Input, FormItem } from '@/components/m'
+import Img from '@/assets/images/auth.png'
+</script>
+<template>
+  <main class="flex justify-center bg-foreground-primary text-white min-h-screen">
+    <section class="w-1/3 py-10 px-10 h-full">
+      <div class="mb-16">
+        <div class="text-3xl font-semibold">Welcome Back 👋</div>
+        <div class="text-white">We are happy to have you back</div>
+      </div>
+      <form class="space-y-3">
+        <FormItem id="email" label="email" class="w-full">
+          <Input
+            id="email"
+            type="email"
+            color="default"
+            placeholder="Enter your Email"
+            class="w-full"
+          />
+        </FormItem>
+        <FormItem id="password" label="password" class="w-full">
+          <Input
+            id="password"
+            type="password"
+            color="default"
+            placeholder="Enter your password"
+            class="w-full"
+          />
+        </FormItem>
+        <div>
+          <Button class="block w-full mt-5" color="primary">Login</Button>
+        </div>
+      </form>
+    </section>
+    <section class="flex-1 h-full p-5">
+      <div class="rounded-lg overflow-hidden">
+        <img class="w-full object-cover" :src="Img" alt="" />
+      </div>
+    </section>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/router/index.ts
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+import { setupLayouts } from 'virtual:generated-layouts'
+import generatedRoutes from '~pages'
+
+const routes = setupLayouts(generatedRoutes)
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+export default router
